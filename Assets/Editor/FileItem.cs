49fb8017e53c38e9ddc0ab54b2138dc0121e567a
minddrop <ryooooooo1@gmail.com>
2019-12-01 23:47:12 +0900

    fixed format and added namespace

--- Assets/Editor/FileItem.cs
+++ Assets/Editor/FileItem.cs
@@ -2,18 +2,18 @@
 using UnityEditor;
 using System.Diagnostics;
 
-public class FileItem
-{
-  [MenuItem("File/Restart")]
-  static void RestartUnityEditor()
-  {
-    string shPath = Application.dataPath + "/Editor/restart_unity_editor.sh";
+namespace UnityEditorExtension {
+  public class FileItem {
+    [MenuItem("File/Restart")]
+    static void RestartUnityEditor() {
+      string shPath = Application.dataPath + "/Editor/restart_unity_editor.sh";
 
-    string unityEditorPath = EditorApplication.applicationPath;
-    string args = "-projectPath " + Application.dataPath.Replace("/Assets", "");
-    string command = unityEditorPath + " " + args;
+      string unityEditorPath = EditorApplication.applicationPath;
+      string args = "-projectPath " + Application.dataPath.Replace("/Assets", "");
+      string command = unityEditorPath + " " + args;
 
-    Process.Start("/bin/bash", $"-c \"{shPath} {command}\"");
-    EditorApplication.Exit(0);
+      Process.Start("/bin/bash", $"-c \"{shPath} {command}\"");
+      EditorApplication.Exit(0);
+    }
   }
 }
