bca1963267bf2835c342845c0da96ecb95c124b6
minddrop <ryooooooo1@gmail.com>
2019-12-05 21:45:47 +0900

    created simple playercontroller

---
+++ Assets/Scripts/StrikerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StrikerController : MonoBehaviour {
+  Rigidbody rb;
+  Vector3 right;
+  Vector3 left;
+  float inputHorizontal;
+  float inputVertical;
+  void Start() {
+    rb = this.GetComponent<Rigidbody>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    inputHorizontal = Input.GetAxisRaw("Horizontal");
+    inputVertical = Input.GetAxisRaw("Vertical");
+    rb.velocity = Camera.main.transform.right * inputHorizontal;
+  }
+}
